8109000809a82aeb8bbfea11c8fe8a43816002b3
sellauno <sellauno03@gmail.com>
2021-03-26 10:19:56 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sqlite_belanja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sqlite_belanja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+//kode utama Aplikasi tampilan awal
+import 'package:flutter/material.dart';
+import 'package:sqlite_belanja/homepage.dart';
+
+//package letak folder Anda
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tambahkan Item',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Home(),
+    );
+  }
+}
